9de6ed9df9b4471d2a0f63a6f7bf27a9cd01b886
Sharon Osterbind <sosterbind@gmail.com>
2019-05-05 17:35:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,14 +1,10 @@
 
 def introduction(name)
-  "Hi my name is #{name}"
+  puts "Hi my name is #{name}."
 end
 
-puts introduction('sharon')
 
-# describe "#introduction_with_language" do 
-#   it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do 
-#     expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
-#   end
 
 def introduction_with_language(name, language)
-  
+  puts "Hi my name is #{name} and I am learning to program in #{language}."
+end
